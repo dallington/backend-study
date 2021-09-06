9f4a3c9f44b52f17d57322f26fd2db409275b8f9
Dallington Augusto <contato@dallington.com.br>
2021-09-05 21:01:10 -0300

    fix(main): change name PORT to port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT'));
+  await app.listen(configService.get('port'));
 }
 bootstrap();
